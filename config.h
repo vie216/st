91d574561aa4c248c481435218f81581acb17b32
vie <vie.devr@gmail.com>
2024-01-15 03:28:46 +0300

    changed appearance

--- config.h
+++ config.h
@@ -94,37 +94,35 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.6;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-  "green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#10101a",
+	"#df2424",
+  "#24df24",
+	"#dfdf24",
+	"#4f4fdf",
+	"#7c51b9",
+	"#309aba",
+	"#a0a0aa",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#40404a",
+	"#ff4444",
+	"#44ff44",
+	"#ffff44",
+	"#6f6fff",
+	"#9c71d9",
+	"#50bada",
+	"#efeff9",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	"#a6aadc", /* default foreground colour */
+	"#14171e", /* default background colour */
 };
 
 
@@ -132,10 +130,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 258;
-static unsigned int defaultrcs = 259;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 256;
+static unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
