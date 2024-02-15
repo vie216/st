093365a1e35d023df711f090f92afaf3edde1fd0
vie <vie.devr@gmail.com>
2024-02-15 14:29:38 +0300

    Change alpha

--- config.h
+++ config.h
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.85;
+float alpha = 1.0;
 
 /* Terminal colors (16 first used in escape sequence) */
 /* Theme is a modified version of Neutron from https://gogh-co.github.io/Gogh */
